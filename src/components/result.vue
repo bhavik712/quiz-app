e23948dc7d7204940320f6ae60be4ec095ac8ec7
bhavik712 <baradbhavik41@gmail.com>
2023-08-07 14:04:18 +0530

    Quiz app

---
+++ src/components/result.vue
@@ -0,0 +1,15 @@
+<template>
+   <div class="result-box">
+        <div class="result"> You have given {{ correctAnswers }} answers correctly.</div>
+        <div class="desc" v-for="(result,index) in resultList" v-show="index === correctAnswers">{{ result.desc }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Result',
+        props:['resultList','correctAnswers']
+        
+    }
+</script>
+
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import './style/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+const vm = createApp(App)
+
+vm.mount('#app')
