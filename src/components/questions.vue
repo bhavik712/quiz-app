ceda577980d57e71dfe3deaeae11b4771c170466
bhavik712 <baradbhavik41@gmail.com>
2023-08-09 10:18:43 +0530

    refactored Question components

--- src/components/questions.vue
+++ src/components/questions.vue
@@ -1,19 +1,30 @@
 <template>
-    <transition-group name="fade">
-        <div class="question-box" v-for=" (question,qi) in questionList" :key="question.q" v-show="qi == questionAnswered">
-            <div class="current-question">{{ question.q }}</div>
-            <div class="answers" v-for="(option,oi) in question.answers" :key="option.text">
-                <div class="answer" @click="$emit('selectedAnswer', option.is_correct )">{{ option.text }}</div>
-            </div>
+  <transition name="fade">
+    <div class="question-box">
+      <div class="current-question">{{ getQuestion.q }}</div>
+      <div
+        class="answers"
+        v-for="option in getQuestion.answers"
+        :key="option.text"
+      >
+        <div class="answer" @click="$emit('selectedAnswer', option.is_correct)">
+          {{ option.text }}
         </div>
-    </transition-group>
+      </div>
+    </div>
+  </transition>
 </template>
 
 <script>
-    export default {
-        name:'Question',
-        props:['questionList','questionAnswered'],
-        emits: ['selectedAnswer']
-    }
-</script>
+export default {
+  name: "Question",
+  props: ["questionList", "questionAnswered"],
+  emits: ["selectedAnswer"],
 
+  computed: {
+    getQuestion() {
+      return this.questionList[this.questionAnswered];
+    },
+  },
+};
+</script>
